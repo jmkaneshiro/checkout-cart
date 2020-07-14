3da24cd886d094a60256e739b28b3e157074a0de
jmkaneshiro <jmkaneshiro@gmail.com>
2020-07-14 19:30:05 -0400

    Adds Product model/controller/views ## Summary A shop owner can now create, edit, view, and index products. This user can provide a product with a name, description, and price.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,51 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render :index
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+
+    if @product
+      render :show
+    else
+      redirect_to products_url
+    end
+  end
+
+  def new
+    @product = Product.new
+    render :new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to product_url(@product)
+    else
+      render :new 
+    end
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+
+    if @product.update_attributes(product_params)
+      redirect_to product_url(@product)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :price)
+  end
+end
