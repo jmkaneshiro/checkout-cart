1e7d50b44018aa0455e3d43f0bd4d813e6ab7c07
jmkaneshiro <jmkaneshiro@gmail.com>
2020-07-15 18:07:49 -0400

    Sets /products as root

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :products, only: [:index, :show, :new, :create, :edit, :update]
   resources :inventory_items, only: [:index, :show, :create, :destroy]
+
+  root 'products#index'
 end
