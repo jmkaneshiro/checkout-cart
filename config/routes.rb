c08917d9935faca660b243e5426fe29cbcdf5321
jmkaneshiro <jmkaneshiro@gmail.com>
2020-07-17 16:38:59 -0400

    Moves all admin features into admin directory ## Summary Creating products and adding inventory are features that only an admin user should be accessing. All of those features have been moved to an admin directory.
    ## Notes
    The root currently points to /products which only returns a JSON list of available products. This will be updated in the next PR.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :products, only: [:index, :show, :new, :create, :edit, :update]
-  resources :inventory_items, only: [:index, :show, :create, :destroy]
-
+  namespace :admin do
+    resources :products, only: [:index, :show, :new, :create, :edit, :update]
+    resources :inventory_items, only: [:index, :show, :create, :destroy]
+    root 'products#index'
+  end
+  resources :products, only: [:index, :get]
+  
   root 'products#index'
 end
